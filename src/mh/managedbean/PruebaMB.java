5c0c51accbc42b1b92c8691c06bebc69a9c4e8aa
samz550a cuentas <samz550a.cnt@outlook.com>
2016-08-29 21:26:04 -0500

    inicio2.xhtml2
    Se crea en la página un botón que muestra un diálogo modal y allí un formulario pequeño para insertar un nuevo nombre. Se hace funcionar que vaya hasta el managed Bean.
    
    Se añade el nuevo jar de primefaces.

--- src/mh/managedbean/PruebaMB.java
+++ src/mh/managedbean/PruebaMB.java
@@ -6,6 +6,8 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.jboss.logging.Logger;
+
 import mh.modelo.entidades.Prueba1;
 import mh.negocio.Prueba_LEJBLocal;
 
@@ -15,6 +17,9 @@ public class PruebaMB {
 
 	@EJB Prueba_LEJBLocal Prueba_LEJBLocal;
 	
+	private static final Logger LOG = Logger.getLogger(PruebaMB.class.getName());
+	
+	private String nombre;
 	
 	public List<Prueba1> listar()
 	{
@@ -27,6 +32,28 @@ public class PruebaMB {
 		}	
 		
 		return lista;
+	}
+
+	public void registrarListener()
+	{
+		LOG.info("Entrando a registrar");
+		
+		
+	}
+	//----------------------------------------------
+	
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 	
 	
